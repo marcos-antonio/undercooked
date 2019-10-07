a85d0813d56a4254844de0aa9615c05faf48dabd
marcos antonio martins da silva junior <1727501>
2019-10-07 20:38:22 -0300

    Add contador cozinhamento panela

--- panela.lua
+++ panela.lua
@@ -1,15 +1,19 @@
 local panela = {}
 local panela_mt = {__index = panela};
 
-local objPan, tempoCozinhamento, timerAtual, ingredientes
+local objPan, tempoCozinhamento, timerAtual, ingredientes, textoTempoCoz
+local limiteTempoCozinhamento = 15
 
 function panela.new(_x, _y)
     local newPanela = {}
     objPan = display.newRect(_x, _y, 20, 20)
     objPan:setFillColor(0.75)
     x, y = _x, _y
-    tempoCozinhamento = 5
+    tempoCozinhamento = limiteTempoCozinhamento
     timerAtual = nil
+
+    textoTempoCoz = display.newText(tempoCozinhamento, x, y - 30, native.systemFont, 20)
+    textoTempoCoz:setFillColor( 0, 0, 0 )
 
     ingredientes = {}
 
@@ -20,12 +24,13 @@ local function dimunirTempoCozinhamentoClasse(event)
     local params = event.source.params
     params.f1()
     params.f2()
-
 end
 
 function panela:dimunirTempoCozinhamento()
     tempoCozinhamento = tempoCozinhamento - 1
+    textoTempoCoz.text= tempoCozinhamento
 end
+
 
 function panela:adicionarIngrediente(ingr)
     ingr.x = x
@@ -33,15 +38,16 @@ function panela:adicionarIngrediente(ingr)
     ingr.width = 5
     ingr.height = 5
     table.insert(ingredientes, ingr)
-    tempoCozinhamento = 5
-    if (not timerAtual == nil) then
-        timerAtual.cancel()
+    tempoCozinhamento = limiteTempoCozinhamento
+    print(timerAtual)
+    if (not (timerAtual == nil)) then
+        timer.cancel(timerAtual)
     end
     panela:iniciarDimunirTempoCozinhamentoTimer()
 end
 
 function panela:iniciarDimunirTempoCozinhamentoTimer()
-    if (tempoCozinhamento > 0 and tempoCozinhamento <= 5) then
+    if (tempoCozinhamento > 0 and tempoCozinhamento <= limiteTempoCozinhamento) then
         timerAtual = timer.performWithDelay( 1000, dimunirTempoCozinhamentoClasse)
         timerAtual.params = { f1 = panela.dimunirTempoCozinhamento, f2 = panela.iniciarDimunirTempoCozinhamentoTimer}
     end
