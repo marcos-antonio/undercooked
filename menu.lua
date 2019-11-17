be2eb97b3e87e7608c9e467fdfa3a2996265db46
Júnior <mantonioprof@gmail.com>
2019-11-17 17:18:05 -0300

    Add font new game

--- menu.lua
+++ menu.lua
@@ -37,22 +37,20 @@ function scene:create( event )
 	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
 
 	-- display a background image
-	local background = display.newImageRect( "background.jpg", display.actualContentWidth, display.actualContentHeight )
+	local background = display.newImageRect( "ma.png", display.actualContentWidth, display.actualContentHeight )
 	background.anchorX = 0
 	background.anchorY = 0
 	background.x = 0 + display.screenOriginX
 	background.y = 0 + display.screenOriginY
 
-	-- create/position logo/title image on upper-half of the screen
-	local titleLogo = display.newImageRect( "logo.png", 264, 42 )
-	titleLogo.x = display.contentCenterX
-	titleLogo.y = 100
 
 	-- create a widget button (which will loads level1.lua on release)
 	playBtn = widget.newButton{
 		label="New Game",
 		labelColor = { default={255}, over={128} },
 		default="button.png",
+		font="Advertising_Script_Bold.ttf",
+		fontSize=28,
 		over="button-over.png",
 		width=154, height=40,
 		onRelease = onPlayBtnRelease	-- event listener function
@@ -62,7 +60,6 @@ function scene:create( event )
 
 	-- all display objects must be inserted into group
 	sceneGroup:insert( background )
-	sceneGroup:insert( titleLogo )
 	sceneGroup:insert( playBtn )
 	audio.play( backgroundMusic, { channel=1, loops=-1 } )
 end
